c8f5f56a51ee48ed67e422c1d620750c10ca9430
Siva Kondapi Venkata <kvsivaraman@gmail.com>
2021-08-15 21:58:17 +0530

    Update custom_commands.sh

--- custom_commands.sh
+++ custom_commands.sh
@@ -81,3 +81,7 @@ parse_git_branch() {
 }
 
 #export PS1="\u\[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "
+
+docker_update() {
+  docker service update --force --update-order start-first $1
+}
